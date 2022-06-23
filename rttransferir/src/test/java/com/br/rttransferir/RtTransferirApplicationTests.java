82579708040a172d00b56247ba400ccdf61312c3
Nathália Figueira <ntfigueira2@gmail.com>
2022-06-23 04:13:46 -0300

    projeto semana 01

---
+++ rttransferir/src/test/java/com/br/rttransferir/RtTransferirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rttransferir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtTransferirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
